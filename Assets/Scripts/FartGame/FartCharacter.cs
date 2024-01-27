4d7191bbbcfa3add59a6cec62474d7a839ec8992
Tal Wilfand <talwilfand1@gmail.com>
2024-01-27 15:18:40 -0800

    Player in fart scene

--- Assets/Scripts/FartGame/FartCharacter.cs
+++ Assets/Scripts/FartGame/FartCharacter.cs
@@ -12,23 +12,50 @@ public class FartCharacter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        active = true;
         act = new ActionList();
         int rand = Random.Range(0, 2);
         if(rand == 0)
         {
             waitTimer = Random.Range(0.0f, 3f);
-
         }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!active)
+        {
+            return;
+        }
+        if(waitTimer>0)
+        {
+            waitTimer -= Time.deltaTime;
+            return;
+        }
         act.Update(Time.deltaTime);
         if(act.Empty())
         {
             Vector3 newPos = new Vector3(Random.Range(-10.0f, 10.0f), 2, Random.Range(-6.0f, 6.0f));
-            act.Add(new Act.Move(newPos, gameObject, Random.Range(.9f, 3.3f)));
+            act.Add(new Act.Move(newPos, gameObject, Random.Range(1.4f, 3.3f)));
+            int rand = Random.Range(0, 2);
+            if (rand == 0)
+            {
+                waitTimer = Random.Range(0.0f, 1.5f);
+            }
+        }
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name.Contains("Fart"))
+        {
+            act.Clear();
+
+            Vector3 newPos = new Vector3(Random.Range(-10.0f, 10.0f), 2, Random.Range(-6.0f, 6.0f));
+            act.Add(new Act.Move(newPos, gameObject, Random.Range(1.4f, 3.3f)));
+            waitTimer = Random.Range(0.0f, 1.0f);
+
         }
     }
     public void Stop()
