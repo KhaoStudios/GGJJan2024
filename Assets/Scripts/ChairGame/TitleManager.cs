ac37d14d455d8b7dfa838a564958ba02f5900b74
Conor Brennan <conor.b@digipen.edu>
2024-01-28 15:21:08 -0800

    added a title pop up prefab

--- Assets/Scripts/ChairGame/TitleManager.cs
+++ Assets/Scripts/ChairGame/TitleManager.cs
@@ -4,10 +4,12 @@ namespace ChairGame
 {
     public class TitleManager : MonoBehaviour
     {
+        public string TitleText;
+        
         // Start is called before the first frame update
         void Start()
         {
-            GameManager.Instance.DisplayInfo("Kick-Off Meeting!");
+            GameManager.Instance.DisplayInfo(TitleText);
         }
     }
 }
