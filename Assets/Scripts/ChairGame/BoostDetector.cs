9123ccd6749c49d23782653014f4b5450ff0d1eb
Conor Brennan <conor.b@digipen.edu>
2024-01-27 23:37:44 -0800

    chair spin boosting go brrrrrrrrrrr

--- Assets/Scripts/ChairGame/BoostDetector.cs
+++ Assets/Scripts/ChairGame/BoostDetector.cs
@@ -6,10 +6,17 @@ namespace ChairGame
     public class BoostDetector : MonoBehaviour
     {
         private bool isTouchingWall;
-
+        private GameObject otherPlayer;
+        
         public bool TouchingWall()
         {
             return isTouchingWall;
+        }
+
+        public Rigidbody TouchingPlayer()
+        {
+            if (otherPlayer) return otherPlayer.GetComponent<Rigidbody>();
+            return null;
         }
         
         private void OnTriggerEnter(Collider other)
@@ -17,7 +24,13 @@ namespace ChairGame
             if (other.CompareTag("Wall"))
             {
                 isTouchingWall = true;
-                Debug.Log("touching wall:" + isTouchingWall);
+                Debug.Log("touching wall: true");
+            }
+
+            else if (other.CompareTag("Player"))
+            {
+                otherPlayer = other.gameObject;
+                Debug.Log("touching player: true");
             }
         }
 
@@ -27,6 +40,12 @@ namespace ChairGame
             {
                 isTouchingWall = false;
                 Debug.Log("touching wall:" + isTouchingWall);
+            }
+            
+            else if (other.CompareTag("Player"))
+            {
+                otherPlayer = null;
+                Debug.Log("touching player: false");
             }
         }
     }
